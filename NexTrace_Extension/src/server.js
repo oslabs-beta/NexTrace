cd7fcadc92bc244ec51bf8237d3ec619526019a6
simonL <kedjek177@gmail.com>
2023-10-04 14:17:38 -0400

    server spun for extension

--- NexTrace_Extension/src/server.js
+++ NexTrace_Extension/src/server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.use('/otel', (req, res, next) => {
+  res.locals.trace = req.body;
+  return next();
+}, (req, res) => {
+  return res.status(200).json(res.locals.trace);
+});
+
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
