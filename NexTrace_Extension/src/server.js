710298a432fd7341919fc02977a0818630fa24ab
simonL <kedjek177@gmail.com>
2023-10-04 23:03:06 -0400

    serves array of objects for front end

--- NexTrace_Extension/src/server.js
+++ NexTrace_Extension/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { request } = require('http');
 const app = express();
 const port = 9999;
 
 app.use(express.json());
+
+let requestArray = [];
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -13,25 +16,38 @@ app.use('/otel', (req, res, next) => {
   res.locals.trace = req.body;
   console.log('THE SPAN TRACE', res.locals.trace.resourceSpans[0].scopeSpans[0].spans[0]);
   const span = req.body.resourceSpans[0].scopeSpans[0].spans[0]; 
+  const obj = {name: '', type: '', method: '', duration: 0, status: ''}
+
   if (span) {
     //ENDPOINT - NAME
     const name = span.name;
     console.log('Name:', name)
+    obj.name = name;
     
     //TYPE, METHOD, STATUS_CODE FROM ATTRIBUTES ARRAY
     for (let i = 0; i < span.attributes.length; i++){
-      if(span.attributes[i].key === 'next.span_type') console.log('Type', span.attributes[i].value.stringValue)
-      if(span.attributes[i].key === 'http.method') console.log('Method', span.attributes[i].value.stringValue)
-      if(span.attributes[i].key === 'http.status_code') console.log('Status', span.attributes[i].value.intValue)
+      if(span.attributes[i].key === 'next.span_type') {const type = span.attributes[i].value.stringValue; 
+        console.log('Type', type); 
+        obj.type = type;
+      }
+      if(span.attributes[i].key === 'http.method') {const method = span.attributes[i].value.stringValue; 
+        console.log('Method', method); 
+        obj.method = method;
+      }
+      if(span.attributes[i].key === 'http.status_code') {const status = span.attributes[i].value.intValue; 
+        console.log('Status', status); 
+        obj.status = status;
+      }
     }
     
     //DURATION IN MS DONE
     const duration = (span.endTimeUnixNano  - span.startTimeUnixNano) / 1000000 //converts to milliseconds
     console.log('Duration:', duration, 'ms');
-  
+    obj.duration = duration;
+
   }
-
-
+  requestArray.push(obj)
+  console.log(requestArray);
   return res.status(200).json('Span Received');
 })
 
