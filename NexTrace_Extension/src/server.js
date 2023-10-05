fb321a968e17cfa709a40aad7c56ee6f1d61de89
Jose Velasco <josevelasco1022@gmail.com>
2023-10-04 17:10:21 -0700

    cors package

--- NexTrace_Extension/src/server.js
+++ NexTrace_Extension/src/server.js
@@ -1,29 +1,9 @@
-// const express = require('express');
-// const app = express();
-// const port = 9999;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, world!');
-// });
-
-// app.use('/otel', (req, res, next) => {
-//   res.locals.trace = req.body;
-//   console.log(req.body);
-//   return next();
-// }, (req, res) => {
-//   return res.status(200).json(res.locals.trace);
-// });
-
-
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`);
-// });
-
 const express = require('express');
 const app = express();
 const port = 9999;
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
