c492eacfb50d0ee02d309e93740cc4ebfaf78afb
Charles Dunlap <dunlapviolin@gmail.com>
2023-10-05 16:42:03 -0500

    pushing off before taking new changes

---
+++ NexTrace_Extension/utils/astConstructor.js
@@ -0,0 +1,452 @@
+export default function transformer(file, api) {
+    const j = api.jscodeshift;
+    const ast = j(file.source);
+
+    const rootNode = ast.get().node.program;
+
+    /*
+
+    The following code generates this using jscodeshift:
+         
+     const {
+        trace: trace
+        } = require("@opentelemetry/api");
+
+    */
+
+    const traceRequireStatement = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.objectPattern([
+                    j.property(
+                        'init',
+                        j.identifier('trace'),
+                        j.identifier('trace')
+                    )
+                ]),
+                j.callExpression(
+                    j.identifier('require'),
+                    [j.literal('@opentelemetry/api')]
+                )
+            )
+        ]
+    );
+
+    /*
+
+    The following code generates this using jscodeshift:
+
+    const { OTLPTraceExporter: OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-http');
+
+    */
+
+    const OTLPTraceExporterRequireStatement = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.objectPattern([
+                    j.property(
+                        'init',
+                        j.identifier('OTLPTraceExporter'),
+                        j.identifier('OTLPTraceExporter')
+                    )
+                ]),
+                j.callExpression(
+                    j.identifier('require'),
+                    [j.literal('@opentelemetry/exporter-trace-otlp-http')]
+                )
+            )
+        ]
+    );
+
+    /*
+
+    The following code generates this using jscodeshift:
+    
+    const {
+    BasicTracerProvider: BasicTracerProvider,
+    SimpleSpanProcessor: SimpleSpanProcessor
+    } = require("@opentelemetry/sdk-trace-base");
+
+    */
+
+    const traceBaseRequireStatemt = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.objectPattern([
+                    j.property(
+                        'init',
+                        j.identifier('BasicTracerProvider'),
+                        j.identifier('BasicTracerProvider')
+                    ),
+                    j.property(
+                        'init',
+                        j.identifier('SimpleSpanProcessor'),
+                        j.identifier('SimpleSpanProcessor')
+                    )
+                ]),
+                j.callExpression(
+                    j.identifier('require'),
+                    [j.literal('@opentelemetry/sdk-trace-base')]
+                )
+            )
+        ]
+    )
+
+    /*
+
+    The following code generates this using jscodeshift:
+
+    const collectorOptions = {
+  url: 'http://localhost:9999/otel', 
+};
+
+    */
+
+    const collectorOptionsDeclaration = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.identifier('collectorOptions'),
+                j.objectExpression(
+                    [
+                        j.property(
+                            'init',
+                            j.identifier('url'),
+                            j.literal('http://localhost:9999/otel')
+                        )
+                    ]
+                )
+            )
+        ]
+    )
+
+    /*
+
+    The following code generates this using jscodeshift:
+
+    const provider = new BasicTracerProvider();
+
+    */
+
+    const providerDeclarationStatement = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.identifier('provider'),
+                j.newExpression(
+                    j.identifier('BasicTracerProvider'),
+                    []
+                )
+            )
+        ]
+    )
+
+    /*
+
+    The following code generates this using jscodeshift:
+
+    const exporter = new OTLPTraceExporter(collectorOptions);
+
+    */
+
+    const exporterDeclarationStatement = j.variableDeclaration(
+        'const',
+        [
+            j.variableDeclarator(
+                j.identifier('exporter'),
+                j.newExpression(
+                    j.identifier('OTLPTraceExporter'),
+                    [j.identifier('collectorOptions')]
+                )
+            )
+        ]
+    )
+
+    /*
+ 
+    The following code generates this using jscodeshift:
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+    */
+
+    const setGlobalTracerStatement = j.expressionStatement(
+        j.callExpression(
+            j.memberExpression(
+                j.identifier('trace'), j.identifier('setGlobalTracerProvider')
+            ),
+            [j.identifier('provider')]
+        )
+    )
+
+    /*
+    
+    The following code generates this using jscodeshift:
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+    */
+
+    const addSpanProcessorStatement = j.expressionStatement(
+        j.callExpression(
+            j.memberExpression(
+                j.identifier('provider'), j.identifier('addSpanProcessor')
+            ),
+            [
+                j.newExpression(
+                    j.identifier('SimpleSpanProcessor'),
+                    [
+                        j.identifier('exporter')
+                    ]
+                )
+            ]
+        )
+    )
+
+    /*
+    
+    The following code generates this using jscodeshift:
+
+    provider.register();
+
+    */
+
+    const providerRegisterStatement = j.expressionStatement(
+        j.callExpression(
+            j.memberExpression(
+                j.identifier('provider'), j.identifier('register')
+            ), []
+        )
+    )
+
+    /*
+
+    The following code generates this using jscodeshift:
+    
+    fetch("http://localhost:9999", {
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            log: content
+        })
+    });
+
+    */
+
+    const fetchStatement = j.expressionStatement(
+        j.callExpression(
+            j.identifier('fetch'),
+            [
+                j.literal('http://localhost:9999'),
+                j.objectExpression([
+                    j.property(
+                        'init',
+                        j.identifier('method'),
+                        j.literal('POST')
+                    ),
+                    j.property(
+                        'init',
+                        j.identifier('headers'),
+                        j.objectExpression([
+                            j.property(
+                                'init',
+                                j.literal('Content-Type'),
+                                j.literal('application/json')
+                            )
+                        ])
+                    ),
+                    j.property(
+                        'init',
+                        j.identifier('body'),
+                        j.callExpression(
+                            j.memberExpression(
+                                j.identifier('JSON'),
+                                j.identifier('stringify')), [
+                            j.objectExpression([
+                                j.property(
+                                    'init',
+                                    j.identifier('log'),
+                                    j.identifier('content')
+                                )
+                            ])
+                        ]
+                        )
+                    )
+                ])
+            ]
+        )
+    )
+
+    /*
+
+    The following code generates this using jscodeshift:
+
+    function captureAndSend(...args) {
+        const content = args.map(arg => JSON.stringify(arg));
+
+        fetch("http://localhost:9999", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                log: content
+            })
+        });
+    }
+
+    */
+
+    const dispatchFunctionStatement = j.functionDeclaration(
+        j.identifier('captureAndSend'),
+        [j.restElement(j.identifier('args'))],
+        j.blockStatement([
+            j.variableDeclaration(
+                'const',
+                [
+                    j.variableDeclarator(
+                        j.identifier('content'),
+                        j.callExpression(
+                            j.memberExpression(
+                                j.identifier('args'),
+                                j.identifier('map')
+                            ),
+                            [
+                                j.arrowFunctionExpression(
+                                    [j.identifier('arg')],
+                                    j.callExpression(
+                                        j.memberExpression(j.identifier('JSON'), j.identifier('stringify')),
+                                        [j.identifier('arg')]
+                                    )
+                                )
+                            ]
+                        )
+                    )
+                ]
+            ),
+            fetchStatement
+        ])
+    );
+
+
+    const processExpression = (node) => {
+        if (node.type === 'Literal') {
+            return node;
+        }
+        else if (node.type === 'Identifier') {
+            return node.name;
+        }
+        else if (node.type === 'BinaryExpression') {
+            const left = processExpression(node.left);
+            const operator = node.operator;
+            const right = processExpression(node.right);
+            return j.binaryExpression(node.operator, processExpression(node.left), processExpression(node.right));
+        }
+        else if (node.type === 'CallExpression') {
+            const callee = processExpression(node.callee);
+            const args = node.arguments.map(processExpression).join(', ');
+            return j.callExpression(processExpression(node.callee), node.arguments.map(processExpression));
+        }
+        else if (node.type === 'MemberExpression') {
+            const object = processExpression(node.object);
+            const property = node.computed ? `[${processExpression(node.property)}]` : `.${processExpression(node.property)}`;
+            return `${object}${property}`;
+        }
+    }
+
+    /*
+
+    The following code is used in the ast.find() to create captureAndSend(<ARGUMENTS GO HERE>)
+
+    */
+
+    function createCaptureAndSendInvocation(args) {
+        return j.expressionStatement(
+            j.callExpression(
+                j.identifier('captureAndSend'),
+                args
+            )
+        )
+    }
+
+    //Helper function to append found content with new content. 'statementPath' refers to an individual node path, such as in the ast.find().
+    function insertContentAfter(statementPath, newContent) {
+
+        let parentPath = statementPath;
+        while (parentPath && parentPath.node.type !== 'ExpressionStatement') {
+            parentPath = parentPath.parentPath;
+        }
+
+        if (parentPath && parentPath.node.type === 'ExpressionStatement') {
+            parentPath.insertAfter(newContent);
+        }
+    }
+
+    // ast.find(j.CallExpression, {
+    //     callee: {
+    //         type: "MemberExpression",
+    //         object: { type: "Identifier", name: "console" },
+    //         property: { type: "Identifier", name: "log" },
+    //     }
+    // }).forEach(log => {
+    //     const logArguments = log.node.arguments;
+    //     //Create a new instance of invoking captureAndSend
+    //     const funcExpression = createCaptureAndSendInvocation(logArguments);
+    //     insertContentAfter(log, funcExpression);
+    // })
+
+    rootNode.body.unshift(collectorOptionsDeclaration);
+    // rootNode.body.unshift(providerRegisterStatement);
+    // rootNode.body.unshift(dispatchFunctionStatement);
+    // rootNode.body.unshift(addSpanProcessorStatement);
+    // rootNode.body.unshift(setGlobalTracerStatement);
+    // rootNode.body.unshift(exporterDeclarationStatement);
+    // rootNode.body.unshift(providerDeclarationStatement);
+    // rootNode.body.unshift(traceBaseRequireStatemt);
+    // rootNode.body.unshift(OTLPTraceExporterRequireStatement);
+    // rootNode.body.unshift(traceRequireStatement);
+
+    return ast.toSource();
+}
+
+
+/*
+
+---BELOW IS THE STRATEGY---
+
+(***user logs console***)
+const arg1 = 'hello world.';
+console.log(arg1);
+
+(***we capture it and dispatch it to our helper function***)
+captureAndSend(arg1) // --> hello world.
+
+
+---THE BOILER-PLATE CODE WE WANT TO PREPEND---
+
+const { trace } = require('@opentelemetry/api');
+const {
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} = require('@opentelemetry/sdk-trace-base');
+ const provider = new BasicTracerProvider();
+ provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));  ---WAIT FOR THIS ONE
+ trace.setGlobalTracerProvider(provider);
+
+*/
+
+//Babel parser.
+//npx jscodeshift -t utils/astConstructor.js utils/testFile.ts --parser=babel
+
+//If the babel parser fails, try the ts parser.
+//npx jscodeshift -t utils/astConstructor.js utils/testFile.ts --parser=ts
