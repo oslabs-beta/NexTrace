ea21d3da6103abbca6d322ee349f72c0e6c82911
simonL <kedjek177@gmail.com>
2023-10-05 23:02:32 -0400

    button functions

--- NexTrace_Extension/extension.js
+++ NexTrace_Extension/extension.js
@@ -9,41 +9,45 @@ function activate(context) {
 
   //PRIMARY PANEL FOR HTTP REQUESTS W/ METRICS & GRAPHS
   try {
-  const panel = vscode.window.createWebviewPanel(
-    'nextrace-display', // Identifies the type of the webview. Used internally
-    'NexTrace Display', // Title of the panel displayed to the user
-    vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-    {
-      enableScripts: true,
-      localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'react-app'))]
-    } // Webview options. More on these later.
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand('NexTrace.openTable', () => {
+        const panel = vscode.window.createWebviewPanel(
+          'nextrace-display', // Identifies the type of the webview. Used internally
+          'NexTrace Display', // Title of the panel displayed to the user
+          vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+          {
+            enableScripts: true,
+            localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'react-app'))]
+          } // Webview options. More on these later.
+        );
+        const reactAppPath = path.join(context.extensionPath, 'react-app', 'dist', 'bundle.js');
+        const reactAppUri = panel.webview.asWebviewUri(vscode.Uri.file(reactAppPath));
+      
+        const cssAppPath = path.join(context.extensionPath, 'react-app', 'src', 'style.css');
+        const cssAppUri = panel.webview.asWebviewUri(vscode.Uri.file(cssAppPath)); //.with({ scheme: 'vscode-webview-resource' })
+        // <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'self' https://*.vscode-cdn.net vscode-webview-resource:; script-src-elem 'unsafe-inline' 'self' https://*.vscode-cdn.net vscode-webview-resource:;">
+      
+        const webviewContent = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' vscode-webview-resource: 'unsafe-inline'; style-src-elem 'self' vscode-webview-resource: 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://*.vscode-cdn.net vscode-webview-resource:;">
+              <link rel="stylesheet" type="text/css" href="${cssAppUri}">
+          </head>
+          <body>
+              <div id="root"></div>
+              <h1>Hello World!</h1>
+              <script src="${reactAppUri}"></script>
+          </body>
+          </html>
+        `;
+      
+        panel.webview.html = webviewContent;
 
-  const reactAppPath = path.join(context.extensionPath, 'react-app', 'dist', 'bundle.js');
-  const reactAppUri = panel.webview.asWebviewUri(vscode.Uri.file(reactAppPath));
-
-  const cssAppPath = path.join(context.extensionPath, 'react-app', 'src', 'style.css');
-  const cssAppUri = panel.webview.asWebviewUri(vscode.Uri.file(cssAppPath)); //.with({ scheme: 'vscode-webview-resource' })
-  // <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'self' https://*.vscode-cdn.net vscode-webview-resource:; script-src-elem 'unsafe-inline' 'self' https://*.vscode-cdn.net vscode-webview-resource:;">
-
-  const webviewContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' vscode-webview-resource: 'unsafe-inline'; style-src-elem 'self' vscode-webview-resource: 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://*.vscode-cdn.net vscode-webview-resource:;">
-        <link rel="stylesheet" type="text/css" href="${cssAppUri}">
-    </head>
-    <body>
-        <div id="root"></div>
-        <h1>Hello World!</h1>
-        <script src="${reactAppUri}"></script>
-    </body>
-    </html>
-  `;
-
-  panel.webview.html = webviewContent;
+      })
+    );
   
   }
   catch(err) {
--- NexTrace_Extension/react-sidepanel/src/components/table.jsx
+++ NexTrace_Extension/react-sidepanel/src/components/table.jsx
@@ -3,25 +3,36 @@ import React from 'react';
 
 
 export default function Table() {
-    const handleClick = () => {
+    const handleClick = (cmd) => {
         // Get the vscode object
         // const vscode = acquireVsCodeApi();
-        console.log('IM CLICKING RN!!!!!!!')
-        console.log(vscode);
+        console.log('IM CLICKING RN!!!!!!!')        
         
         // Send a message to your extension with the command
-        vscode.postMessage('NexTrace.startServer');
+        if(cmd === 'startServer') vscode.postMessage('NexTrace.startServer');
+        if(cmd === 'openMetrics') vscode.postMessage('NexTrace.openTable');
       };
     
-    
+    let filePath;
+
+    const check = () => {
+        filePath = document.getElementById("myFile").value;
+        console.log(filePath);
+    }
 
     return (
         <div className='panel'>
-            <button className='startButton' onClick={e => {handleClick()}}>Start</button>
+            <button className='startButton' onClick={e => {handleClick('startServer')}}>Start</button>
             <p>NexTrace running on port: 3695....</p>
 
-            <button className='buttonOne'></button>
-            <button className='buttonOne'></button>
+            <input type="file" id="myFile" name="filename"></input>
+
+
+            <input type="file" id="myFile" name="filename" disabled />
+
+
+            <button className='buttonOne'  onClick={e => {handleClick('openMetrics')}}></button>
+            <button className='buttonOne'  onClick={e => {check()}}></button>
             <button className='buttonOne'></button>
             <button className='buttonOne'></button>
             <button className='buttonOne'></button>
