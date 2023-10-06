f05de029db2f438eed55d83c08446bbf7b461d4d
Charles Dunlap <dunlapviolin@gmail.com>
2023-10-06 00:00:28 -0500

    correctly mutates file based on user input

--- NexTrace_Extension/react-sidepanel/src/components/table.jsx
+++ NexTrace_Extension/react-sidepanel/src/components/table.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Table() {
+    const [filePath, setFilePath] = useState('')
+
+    function handleFile() {
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                const fileContent = event.target.result;
+                const vscode = window.vscodeApi;
+                vscode.postMessage({ command: 'transformCode', path: file.path });
+            }
+            reader.readAsText(file);
+        }
+    }
 
     return (
         <div className='panel'>
-            <button className='startButton'>Start</button>
+            <label htmlFor='file-path'>File Path:</label>
+            <input id='file-path' type='text' value={filePath} onChange={(e) => setFilePath(e.target.value)}></input>
+            <button className='startButton' onClick={handleFile}>Start</button>
+            <input type='file' id='fileInput' />
             <p>NexTrace running on port: 3695....</p>
 
             <button className='buttonOne'></button>
