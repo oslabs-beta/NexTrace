b05834b5ad61ebf2b2bfac2ec20d5c82981755e0
Connor Keyes <connorkeyes@Connors-MacBook-Air.local>
2023-10-07 14:35:44 -0700

    configure state for file modification

--- NexTrace_Extension/react-sidepanel/src/components/table.jsx
+++ NexTrace_Extension/react-sidepanel/src/components/table.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 export default function Table() {
+    const [filePath, setFilePath] = useState('')
+    const vscode = window.vscodeApi;
+
+    const handleClick = (cmd) => {
+        console.log('IM CLICKING RN!!!!!!!')        
+        
+        // Send a message to your extension with the command
+        if(cmd === 'startServer') vscode.postMessage('NexTrace.startServer');
+        if(cmd === 'openMetrics') vscode.postMessage('NexTrace.openTable');
+      };
+
+    function handleFile() {
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                const fileContent = event.target.result;
+                vscode.postMessage({ command: 'transformCode', path: file.path });
+            }
+            reader.readAsText(file);
+        }
+    }
 
     return (
         <div className='panel'>
-            <button className='startButton'>Start</button>
+            <button className='startButton' onClick={e => {handleClick('startServer')}}>Start</button>
             <p>NexTrace running on port: 3695....</p>
 
-            <button className='buttonOne'></button>
-            <button className='buttonOne'></button>
+            <input type="file" id="myFile" name="filename"></input>
+            <button className='buttonOne'  onClick={e => {handleClick('openMetrics')}}></button>
+            <button className='buttonOne'  onClick={e => {}}></button>
             <button className='buttonOne'></button>
             <button className='buttonOne'></button>
             <button className='buttonOne'></button>
@@ -16,3 +42,4 @@ export default function Table() {
         </div>
     )
 }
+
