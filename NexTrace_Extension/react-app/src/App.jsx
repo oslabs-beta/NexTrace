bac4dd7c63bcdfa4cc36c1e7071276a00b09bcdc
Jose Velasco <josevelasco1022@gmail.com>
2023-10-07 14:34:14 -0700

    refactored table to async/await to thenable

--- NexTrace_Extension/react-app/src/App.jsx
+++ NexTrace_Extension/react-app/src/App.jsx
@@ -22,23 +22,10 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-// const rows = [
-//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//     createData('Eclair', 262, 16.0, 24, 6.0),
-//     createData('Cupcake', 305, 3.7, 67, 4.3),
-//     createData('Gingerbread', 356, 16.0, 49, 3.9),
-//   ];
-
 
 function createNewData(name, status, method, type, duration, rendering) {
     return { name, status, method, type, duration, rendering };
@@ -46,35 +33,34 @@ function createNewData(name, status, method, type, duration, rendering) {
   
   export default function CustomizedTables() {
     const [awaitedData, setAwaitedData] = useState([]);
-  
+
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          await new Promise(resolve => setTimeout(resolve, 20000));
-  
-          const response = await fetch('http://localhost:3695/getData');
-  
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-  
-          const data = await response.json();
-  
-          const transformedData = data.map((arr) =>
-            createNewData(arr.name, arr.status, arr.method, arr.type, arr.duration, arr.rendering)
-          );
-  
-          setAwaitedData(transformedData);
-  
-          console.log('Request Data:', data);
-          console.log('Transformed Data:', transformedData);
-        } catch (error) {
-          console.error('Error:', error);
-        }
+      const fetchData = () => {
+        setTimeout(() => {
+          fetch('http://localhost:3695/getData')
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network error');
+              }
+              return response.json();
+            })
+            .then(data => {
+              const transformedData = data.map(arr =>
+                createNewData(arr.name, arr.status, arr.method, arr.type, arr.duration, arr.rendering)
+              );
+              setAwaitedData(transformedData);
+              console.log('Request Data:', data);
+              console.log('Transformed Data:', transformedData);
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        }, 10000); 
       };
-  
+    
       fetchData();
-    }, []); 
+    }, []);
+    
   
     return (
       <TableContainer component={Paper}>
