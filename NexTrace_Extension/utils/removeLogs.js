a1be9b407b8580851a55536949e0cc484125b921
simonL <kedjek177@gmail.com>
2023-10-24 22:41:18 -0400

    removed test file, further cleanup

--- NexTrace_Extension/utils/removeLogs.js
+++ NexTrace_Extension/utils/removeLogs.js
@@ -72,35 +72,3 @@ const removeLogs = (file, api, i) => {
 }
 
 module.exports = { removeLogs };
-
-
-// /*
-
-// The code below erases this line:
-
-// function captureAndSend(...args) {
-// const content = args.map(arg => JSON.stringify(arg));
-
-// fetch("http://localhost:3695", {
-//     method: "POST",
-
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-
-//     body: JSON.stringify({
-//         log: content
-//         })
-//     });
-// }
-
-// */
-
-// ast.find(j.FunctionDeclaration, {
-//     id: {
-//         type: "Identifier",
-//         name: "captureAndSend"
-//     }
-// }).forEach(path => {
-//     j(path).remove();
-// });
